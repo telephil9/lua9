bcb673c1dfc57e815e068485c7ba5e2a414fe0de
phil9 <telephil9@gmail.com>
2022-12-11 14:21:30 +0000

    remove old unused headers

--- llib9.h
+++
@@ -1,6 +0,0 @@
-#ifndef LLIB9_H__
-#define LLIB9_H__
-
-int openlib9(lua_State *L);
-
-#endif
--- utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H__
-#define UTILS_H__
-
-void pushglobal(lua_State *L, const char *name, int value);
-void createmetatable(lua_State *L, const char *name, luaL_Reg *funcs);
-
-#endif
